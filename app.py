415d10978dc9bca2a9b26b24df4ea8d9ad65c75f
2020009-CPE <90688553+2020009-CPE@users.noreply.github.com>
2023-11-14 12:47:27 +0800

    P3pDec
    Count Added

--- app.py
+++ app.py
@@ -1,37 +1,109 @@
+import cv2
+import numpy as np
 from flask import Flask, render_template, Response
 from ultralytics import YOLO
-import cv2
+import mysql.connector
 
 app = Flask(__name__)
 
-# Load YOLO model
-model = YOLO('best.pt')
+# Load YOLOv8 model
+model = YOLO('peopledetect.pt')
 
-# Open camera
-cap = cv2.VideoCapture(1)
-show_live_camera = True  # Flag to toggle between live camera and uploaded content
+# Initialize MySQL database connection
+conn = mysql.connector.connect(
+    host='localhost',  # Replace with your MySQL server host
+    user='root',  # Replace with your MySQL username
+    password='',  # Replace with your MySQL password
+    database='peoplecount'  # Replace with your database name
+)
+cursor = conn.cursor()
 
-def generate_frames():
-    while show_live_camera:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            results = model.track(frame, persist=True)
-            frame = results[0].plot()
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# Create a table to store the counts if it doesn't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS people_counts (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    person_in INT,
+                    person_out INT,
+                    total_count INT
+                )''')
+
+class VideoCamera:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(3)  # Change camera index if needed
+        self.people_counter = PeopleCounter()
+
+    def __del__(self):
+        self.cap.release()
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+
+        # Detect and count people
+        results = model.track(frame, persist=True)
+        count_in, count_out, total_count = self.people_counter.count_people(results)
+
+        # Visualize bounding boxes
+        frame_ = results[0].plot()
+
+        # Add the counts below the bounding boxes with white text
+        text = f"Total: {total_count}, In: {count_in}, Out: {count_out}"
+        cv2.putText(frame_, text, (10, frame_.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)  # White color
+
+        # Insert counts into the database
+        self.people_counter.insert_counts_to_db()
+
+        ret, jpeg = cv2.imencode('.jpg', frame_)
+        return jpeg.tobytes()
+
+class PeopleCounter:
+    def __init__(self):
+        self.count_in = 0
+        self.count_out = 0
+        self.total_count = 0
+
+    def count_people(self, results):
+        self.count_in = 0
+        self.count_out = 0
+
+        for box in results[0].boxes:
+            cords = box.xyxy[0].tolist()
+            cords = [round(x) for x in cords]
+            class_id = results[0].names[box.cls[0].item()]
+
+            # Extract the color based on class_id
+            color = (0, 0, 0)  # Default color (black)
+
+            if class_id == "PEOPLEIN":
+                color = (0, 165, 255)  # Orange for PEOPLEIN
+                self.count_in += 1
+            elif class_id == "PEOPLEOUT":
+                color = (0, 255, 255)  # Yellow for PEOPLEOUT
+                self.count_out += 1
+
+        self.total_count = self.count_in + self.count_out  # Use addition here
+        return self.count_in, self.count_out, self.total_count
+
+    def insert_counts_to_db(self):
+        cursor.execute('''INSERT INTO people_counts (person_in, person_out, total_count)
+                          VALUES (%s, %s, %s)''', (self.count_in, self.count_out, self.total_count))
+        conn.commit()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        if frame:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 @app.route('/video_feed')
 def video_feed():
-    if show_live_camera:
-        return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- main.py
+++ main.py
@@ -1,31 +1,103 @@
-from ultralytics import YOLO
 import cv2
+from ultralytics import YOLO
+import mysql.connector
+from time import sleep
 
-# load yolov8 model
-model = YOLO('best.pt')
+# Load YOLOv8 model
+model = YOLO('peopledetect.pt')
 
-# open camera
-cap = cv2.VideoCapture(0)  # 0 corresponds to the default camera, you can change it if you have multiple cameras
+# Open camera
+cap = cv2.VideoCapture(3)  # 0 corresponds to the default camera, you can change it if you have multiple cameras
 
-# read frames
+# Increase FPS
+fps = 120  # Set a higher value, e.g., 60 FPS
+
+# Initialize MySQL database connection
+conn = mysql.connector.connect(
+    host='localhost',  # Replace with your MySQL server host
+    user='root',  # Replace with your MySQL username
+    password='',  # Replace with your MySQL password
+    database='peoplecount'  # Replace with your database name
+)
+cursor = conn.cursor()
+
+# Create a table to store the counts if it doesn't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS people_counts (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    person_in INT,
+                    person_out INT,
+                    total_count INT
+                )''')
+
+class PeopleCounter:
+    def __init__(self):
+        self.count_in = 0
+        self.count_out = 0
+        self.total_count = 0
+
+    def count_people(self, results):
+        self.count_in = 0
+        self.count_out = 0
+
+        for box in results[0].boxes:
+            cords = box.xyxy[0].tolist()
+            cords = [round(x) for x in cords]
+            class_id = results[0].names[box.cls[0].item()]
+
+            # Extract the color based on class_id
+            color = (0, 0, 0)  # Default color (black)
+
+            if class_id == "PEOPLEIN":
+                color = (0, 165, 255)  # Orange for PEOPLEIN
+                self.count_in += 1
+            elif class_id == "PEOPLEOUT":
+                color = (0, 255, 255)  # Yellow for PEOPLEOUT
+                self.count_out += 1
+
+            # Draw bounding box with color
+            cv2.rectangle(frame, (cords[0], cords[1]), (cords[2], cords[3]), color, 2)
+
+        self.total_count = self.count_in + self.count_out  # Use addition here
+        return self.count_in, self.count_out, self.total_count
+
+    def insert_counts_to_db(self):
+        cursor.execute('''INSERT INTO people_counts (person_in, person_out, total_count)
+                          VALUES (%s, %s, %s)''', (self.count_in, self.count_out, self.total_count))
+        conn.commit()
+
+# Create PeopleCounter instance
+people_counter = PeopleCounter()
+
+# Read frames and detect/count people
 while True:
     ret, frame = cap.read()
 
     if not ret:
         break
 
-    # detect objects
-    # track objects
+    # Detect and count people
     results = model.track(frame, persist=True)
 
-    # plot results
+    # Update and display the counts
+    count_in, count_out, total_count = people_counter.count_people(results)
+
+    # Visualize bounding boxes
     frame_ = results[0].plot()
 
-    # visualize
+    # Add the counts below the bounding boxes with white text
+    text = f"Total: {total_count}, In: {count_in}, Out: {count_out}"
+    cv2.putText(frame_, text, (10, frame_.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)  # White color
+
+    # Visualize
     cv2.imshow('frame', frame_)
+
+    # Insert counts into the database
+    people_counter.insert_counts_to_db()
+
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
-# release the camera and close the window
+# Release the camera and close the window
 cap.release()
 cv2.destroyAllWindows()
